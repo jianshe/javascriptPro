2b6718c6531e27b0fe62a1ea5c9a162acfacb244
zhangjianshe <zhangjianshe@eastmoney.com>
2018-06-11 17:57:24 +0800

    update fiels

--- src/proxy.js
+++ src/proxy.js
@@ -1,37 +1,59 @@
 //proxy实例：关于属性
-// let obj = {
-//     add: function (val) {
-//         return val + 100;
-//     },
-//     name: 'I am jianshe'
-// }
-// let pro = new Proxy({
-//     add: function (val) {
-//         return val + 100;
-//     },
-//     name: 'I am jianshe'
-// }, {
-//     get: function (target, key, property) {
-//         console.log("come in get");
-//         return target[key];
-//     },
-//     set: function (target, key, value, receiver) {
-//         console.log(`setting ${key}=${value}`);
-//         return target[key]=value;
-//     }
-// });
-// console.log(pro.name)
-// pro.name="jianshe";
-// console.log(pro.name);
-//proxy 关于方法
-var target = function(){
-    return "I am jianshe";
+//在目标对象之前架设一层“拦截”，外界对该对象的访问，都必须先通过这层拦截。
+let obj = {
+    add: function(val) {
+        return val + 100;
+    },
+    name: 'I am jianshe'
 }
-var handler = {
-    apply(target, ctx, args) {
-        console.log('do apply');
-        return Reflect.apply(...arguments);
+let pro = new Proxy({
+    add: function(val) {
+        return val + 100;
+    },
+    name: 'I am jianshe'
+}, {
+    get: function(target, key, property) {
+        console.log("come in get");
+        return target[key];
+    },
+    set: function(target, key, value, receiver) {
+        console.log(`setting ${key}=${value}`);
+        return Reflect.set(target, key, value, receiver);
     }
-}
-var pro = new Proxy(target,handler);
-console.log(pro())
+});
+console.log(pro.name)
+pro.name = "jianshe";
+console.log(pro.name);
+
+//proxy 关于方法
+// var target = function() {
+//     return "I am jianshe";
+// }
+// var handler = {
+//     apply(target, ctx, args) {
+//         console.log('do apply');
+//         return Reflect.apply(...arguments);
+//     }
+// }
+// var pro = new Proxy(target, handler);
+// console.log(pro())
+
+
+//eg set方法用来拦截某个属性的赋值操作。
+let validator = {
+    set: function(obj, prop, value) {
+        if (prop === 'age') {
+            if (!Number.isInteger(value)) {
+                throw new TypeError('The age is not an integer');
+            }
+            if (value > 200) {
+                throw new RangeError('The age seems invalid');
+            }
+        }
+        //对于满足条件的age 属性以及其他属性直接保存
+        obj[prop] = value;
+    }
+};
+
+let person = new Proxy({}, validator);
+person.age = 300;
